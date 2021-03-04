fbd032ee28d6432c605c0c1ee547048db85f1430
Gamalliel Sharon <you@example.com>
2021-03-04 17:04:47 +0700

    adding more practice

--- crash_course_python/quiz_string.py
+++ crash_course_python/quiz_string.py
@@ -65,4 +65,198 @@ print(replace_ending("The weather is nice in May", "May", "April"))
 # Should display "The weather is nice in April"
 
 
+# ========================================================================================
+# LISTS
+def get_word(sentence, n):
+	# Only proceed if n is positive 
+	if n > 0:
+		words = sentence.split(" ")
+		# Only proceed if n is not more than the number of words 
+		if n <= len(words) and n>0:
+			return(words[n-1])
+	return("")
+
+print(get_word("This is a lesson about lists", 4)) # Should print: lesson
+print(get_word("This is a lesson about lists", -4)) # Nothing
+print(get_word("Now we are cooking!", 1)) # Should print: Now
+print(get_word("Now we are cooking!", 5)) # Nothing
+# ========================================================================================
+def skip_elements(elements):
+	# Initialize variables
+	new_list = []
+	i = 0
+
+	# Iterate through the list
+	for char in elements:
+		# Does this element belong in the resulting list?
+		if i%2 == 0:
+			# Add this element to the resulting list
+			new_list += [char]
+		# Increment i
+		i+=1
+
+	return new_list
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+print(skip_elements([])) # Should be []
+
+
+# ========================================================================================
+def file_size(file_info):
+	name, type, byte= file_info
+	return("{:.2f}".format(byte / 1024))
+
+print(file_size(('Class Assignment', 'docx', 17875))) # Should print 17.46
+print(file_size(('Notes', 'txt', 496))) # Should print 0.48
+print(file_size(('Program', 'py', 1239))) # Should print 1.21
+
+
+
+# QUIZZ
+# 1
+# Given a list of filenames, we want to rename all the files with extension hpp to the extension h. 
+# To do this, we would like to generate a new list called newfilenames, consisting of the new filenames. 
+# Fill in the blanks in the code using any of the methods you’ve learned thus far, like a for loop or a 
+# list comprehension.
+
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.  
+newfilenames = [e.replace('.hpp','.h') for e in filenames]
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+
+
+# 2
+# Let's create a function that turns text into pig latin: a simple text transformation that modifies each 
+# word moving the first character to the end and appending "ay" to the end. For example, python ends up 
+# as ythonpay.
+
+def pig_latin(text):
+  words = text.split()
+  pigged_text = []
+
+  for word in words:
+    word = word[1:] + word[0] + 'ay'
+    pigged_text.append(word)
+
+  return ' '.join(pigged_text)
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+
+# 3
+# The permissions of a file in a Linux system are split into three sets of three 
+# permissions: read, write, and execute for the owner, group, and others. Each of the three values 
+# can be expressed as an octal number summing each permission, with 4 corresponding to read, 2 to write, 
+# and 1 to execute. Or it can be written with a string using the letters r, w, and x or - when the 
+# permission is not granted.
+# For example: 
+# 640 is read/write for the owner, read for the group, and no permissions for the others; converted to a string, it would be: "rw-r-----"
+# 755 is read/write/execute for the owner, and read/execute for group and others; converted to a string, it would be: "rwxr-xr-x"
+# Fill in the blanks to make the code convert a permission in octal format into a string format.
+
+def octal_to_string(octal):
+    result = ""
+    value_letters = [(4,"r"),(2,"w"),(1,"x")]
+    # Iterate over each of the digits in octal
+    for i in [int(n) for n in str(octal)]:
+        # Check for each of the permissions values
+        for value, letter in value_letters:
+            if i >= value:
+                result += letter
+                i -= value
+            else:
+                result += '-'
+    return result
+    
+print(octal_to_string(755)) # Should be rwxr-xr-x
+print(octal_to_string(644)) # Should be rw-r--r--
+print(octal_to_string(750)) # Should be rwxr-x---
+print(octal_to_string(600)) # Should be rw-------
+
+# 5
+# The group_list function accepts a group name and a list of members, and returns a string with the
+# format: group_name: member1, member2, … For example, group_list("g", ["a","b","c"]) 
+# returns "g: a, b, c". Fill in the gaps in this function to do that.
+def group_list(group, users):
+  members = ', '.join(users)
+  return "{} {}".format(group, members)
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
+
+# 6
+# The guest_list function reads in a list of tuples with the name, age, and profession of each party 
+# guest, and prints the sentence "Guest is X years old and works as __." for each one. For example, 
+# guest_list(('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")) should print out:
+# Ken is 30 years old and works as Chef. Pat is 35 years old and works as Lawyer. Amanda is 25 years old 
+# and works as Engineer. Fill in the gaps in this function to do that. 
+def guest_list(guests):
+	for guest in guests:
+		name, age, job = guest
+		print("{} is {} and works as {}".format(name, age, job))
+
+guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
+
+#Click Run to submit code
+"""
+Output should match:
+Ken is 30 years old and works as Chef
+Pat is 35 years old and works as Lawyer
+Amanda is 25 years old and works as Engineer
+"""
+
+
+# 7
+# The email_list function receives a dictionary, which contains domain names as keys, and a list of users as values. Fill in the blanks to generate a list that contains complete email addresses (e.g. diana.prince@gmail.com).
+def email_list(domains):
+	emails = []
+	for domain, users in domains.items():
+	  for user in users:
+	    emails.append(user+'@'+domain)
+	return(emails)
+
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+
+# 8
+# The groups_per_user function receives a dictionary, which contains group names with the list of users. Users can belong to multiple groups. Fill in the blanks to return a dictionary with the users as keys and a list of their groups as values. 
+
+def groups_per_user(group_dictionary):
+    user_groups = {}
+    for group, users in group_dictionary.items():
+        for user in users:
+            if user not in user_groups:
+                user_groups[user] = []
+            user_groups[user].append(group)
+
+    return(user_groups)
+
+print(groups_per_user({"local": ["admin", "userA"],
+		"public":  ["admin", "userB"],
+		"administrator": ["admin"] }))
+
+# 9
+# The add_prices function returns the total price of all of the groceries in the  dictionary. Fill in the blanks to complete this function.
+
+def add_prices(basket):
+	# Initialize the variable that will be used for the calculation
+	total = 0
+	# Iterate through the dictionary items
+	# for ___:
+		# Add each price to the total calculation
+		# Hint: how do you access the values of
+		# dictionary items?
+		# total += ___
+	# Limit the return value to 2 decimal places
+	return round(total, 2)  
+
+groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59, 
+	"coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
+
+print(add_prices(groceries)) # Should print 28.44
+
+# FINAL QUIZ
 
